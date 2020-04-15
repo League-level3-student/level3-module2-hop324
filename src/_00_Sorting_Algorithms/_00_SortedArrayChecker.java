2bf98b4c72d8d31233bffa21d980690151853431
hop324 <39838785+hop324@users.noreply.github.com>
2020-04-14 20:02:59 -0700

    Sorted Array Checker, Swapping Digits (IP)
    :+1: :sleepy: :mask:

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -7,21 +7,51 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
 
-	
+	static boolean intArraySorted(int[] pop) {
+		int min = 0;
+		for(int i = 0; i < pop.length; i++) {
+			if(pop[i] < min) {
+				return false;
+			}
+			else {
+				min = pop[i];
+			}
+		}
+		return true;
+	}
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
 
-	
+	static boolean doubleArraySorted(double[] goes) {
+		double min = 0;
+		for(int i = 0; i < goes.length; i++) {
+			if(goes[i] < min) {
+				return false;
+			}
+			else {
+				min = goes[i];
+			}
+		}
+		return true;
+	}
 	//3. Write a static method called charArraySorted. 
 	//   This method takes in an array of characters
 	//   and it returns a boolean.
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
-
+	static boolean charArraySorted(char[] the) {
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		for(int i = 0; i < the.length; i++) {
+			if(the[i] != alphabet.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	
 	//4. Write a static method called stringArraySorted. 
 	//   This method takes in an array of Strings
@@ -29,5 +59,27 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
-
+	static boolean stringArraySorted(String[] weasel) {
+		char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		char min = weasel[0].charAt(0);
+		int minTracker = 0;
+		int currentValue = 0;
+		for(int i = 0; i < alphabet.length; i++) {
+			if(min == alphabet[i]) {
+				minTracker = i;
+			}
+		}
+		for(int i = 1; i < weasel.length; i++) {
+			for(int j = 0; j < alphabet.length; j++) {
+				if(weasel[i].charAt(0) == alphabet[j]) {
+					currentValue = j;
+					System.out.println(currentValue);
+				}
+				if(currentValue < minTracker) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,20 +24,51 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		int tracker1 = arr[0];
+		int tracker2 = arr[arr.length-1];
+		arr[0] = tracker2;
+		arr[arr.length-1] = tracker1;
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
+		int tracker = 0;
+		for(int i = 0; i < arr.length; i++) {
+			int min = arr[0];
+			System.out.println(min);
+			System.out.println(arr[i]);
+			if(arr[i] < min) {
+				tracker = arr[i];
+				arr[i] = min;
+				arr[i-1] = tracker;
+				min = tracker;
+				System.out.println(arr[i]);
+				System.out.println(min);
+				System.out.println(arr[i-1]);
+				System.out.println(" ");
+				for(int k =0; k < arr.length; k++) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
 	}
 	
 	//3. Complete the method so that it finds the middle number in the array.
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		int tracker = 0;
+		for(int i = 0; i < arr.length; i++) {
+			int min = arr[0];
+			if(arr[i] < min) {
+				tracker = arr[i];
+				arr[i] = min;
+				min = tracker;
+			}
+		}
+		System.out.println(arr.toString());
+		return arr[arr[arr.length/2]];
 	}
 }
