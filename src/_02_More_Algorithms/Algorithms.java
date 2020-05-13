e5713fa64765e07898b6ff52f626e6ffbe393f40
hop324 <39838785+hop324@users.noreply.github.com>
2020-05-12 19:59:01 -0700

    More and More Tests, More Algorithms (IP)
    :100: :+1: :sunglasses:

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,48 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int j = 0; j < eggs.size(); j++) {
+			System.out.println(eggs.get(j));
+			if(eggs.get(j).contains("cracked")){
+				return j;
+			}
+		}
+		return -1;
 	}
+	
+	public static int countPearls(List<Boolean> ree) {
+		int counter = 0;
+		for(int i = 0; i < ree.size(); i++) {
+			if(ree.get(i)) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	
+	public static double findTallest(List<Double> ree) {
+		Double max = 0.0;
+		for(int i = 0; i < ree.size(); i++) {
+			if(ree.get(i) > max) {
+				max = ree.get(i);
+			}
+		}
+		return max;
+	}
+	
+	public static String findLongestWord(List<String> ree) {
+		String max = "";
+		for(int i =0; i< ree.size(); i++) {
+			if(ree.get(i).length() > max.length()) {
+			max = ree.get(i);	
+			}
+		}
+		return max;
+	}
+	
+	public static boolean containsSOS(List<String> ree) {
+		
+		return true;
+	}
+	
 }
