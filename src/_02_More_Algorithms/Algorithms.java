b7532c2437077937c3d520f9e3e0ef9c6d8e01d7
hop324 <39838785+hop324@users.noreply.github.com>
2020-05-19 19:59:25 -0700

    Finished Mod 2
    :+1: :sunglasses: :100:

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _02_More_Algorithms;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Algorithms {
@@ -44,8 +45,57 @@ public class Algorithms {
 	}
 	
 	public static boolean containsSOS(List<String> ree) {
-		
-		return true;
+		System.out.println(ree);
+		if(ree.contains("... --- ...")){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static List<Double> sortScores(List<Double> tynamoPlz) {
+		double min = 10000;
+		for(int i = 0; i < tynamoPlz.size()-1; i++) {
+			if(tynamoPlz.get(i) < min) {
+				min = tynamoPlz.get(i);
+			}
+		}
+		while(tynamoPlz.get(0) != min)
+		for(int i = 0; i < tynamoPlz.size()-1; i++) {
+			if(tynamoPlz.get(i) > tynamoPlz.get(i+1)) {
+				Double holder = tynamoPlz.get(i);
+				tynamoPlz.set(i, tynamoPlz.get(i+1));
+				tynamoPlz.set(i+1, holder);
+			}
+		}
+		return tynamoPlz;
+	}
+	
+	public static List<String> sortDNA(List<String> DQ){
+		for(int g = 0; g < 100; g++) {
+			for(int i = 0; i < DQ.size()-1; i++) {
+				if(DQ.get(i).length() > DQ.get(i+1).length()) {
+					String holder = DQ.get(i);
+					DQ.set(i, DQ.get(i+1));
+					DQ.set(i+1, holder);
+				}
+			}
+		}
+	return DQ;
+	}
+	
+	public static List<String> sortWords(List<String> Tim) {
+		for(int g = 0; g < 100; g++) {
+			for(int i = 0; i < Tim.size()-1; i++) {
+			if(0 < Tim.get(i).compareTo(Tim.get(i+1))){
+				String holder = Tim.get(i);
+				Tim.set(i, Tim.get(i+1));
+				Tim.set(i+1, holder);
+			}
+			}
+		}
+		return Tim;
 	}
 	
 }
